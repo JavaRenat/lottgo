eab30bbcdb885e015a2c8d5c428439d2312c8b9e
ren <ren_pers@yahoo.com>
2025-01-30 15:42:14 +0100

    add deploy

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -3,7 +3,7 @@ import "./header.scss";
 export default function Header() {
   return (
     <div onClick={() => window.scroll(0, 0)} className="header">
-      BITCOIN's price will predict the winner
+      BITCOIN
     </div>
   );
 }
