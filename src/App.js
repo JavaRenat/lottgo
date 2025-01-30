cffe46f12f280952c6c54a765ed38e7f5c41354a
ren <ren_pers@yahoo.com>
2025-01-30 15:14:23 +0100

    add deploy

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import Header from "./component/header/Header";
 import "./styles.scss";
 import FootNav from "./component/FootNav";
-import { Container } from "@material-ui/core";
-import { Route, Routes } from "react-router-dom";
+import {Container} from "@material-ui/core";
+import {Route, Routes} from "react-router-dom";
 import Trending from "./pages/Trending/Trending";
 import Movies from "./pages/Movies/Movies";
 import Series from "./pages/Series/Series";
 import Search from "./pages/Search/Search";
+
 export default function App() {
-  return (
-    <>
-      <Header />
-      <div className="app">
-        <Container>
-          <Routes>
-            <Route path="/" element={<Trending />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/series" element={<Series />} />
-            <Route path="/search" element={<Search />} />
-          </Routes>
-        </Container>
-      </div>
-      <FootNav />
-    </>
-  );
+    useEffect(() => {
+        console.log("React App загружен!");
+    }, []);
+
+
+    return (
+        <>
+            <Header/>
+            <div className="app">
+                <Container>
+                    <Routes>
+                        <Route path="/" element={<Trending/>}/>
+                        <Route path="/movies" element={<Movies/>}/>
+                        <Route path="/series" element={<Series/>}/>
+                        <Route path="/search" element={<Search/>}/>
+                    </Routes>
+                </Container>
+            </div>
+            <FootNav/>
+        </>
+    );
 }
