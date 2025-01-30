4744e4795d0a6530c93c323b07a77fd84b7b281b
ren <ren_pers@yahoo.com>
2025-01-30 14:50:08 +0100

    add deploy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import "react-alice-carousel/lib/scss/alice-carousel.scss";
+import {HashRouter} from "react-router-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  rootElement
+    <HashRouter>
+        <App/>
+    </HashRouter>,
+    rootElement
 );
